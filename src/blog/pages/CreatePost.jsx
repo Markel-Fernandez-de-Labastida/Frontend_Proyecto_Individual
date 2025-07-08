227c77b97f5ead71f7013b3108ae1d47524979f3
Markel-Fernandez-de-Labastida <markelfdz8@gmail.com>
2025-07-08 14:22:49 +0200

    React Jodit WYSIWYG Editor

--- src/blog/pages/CreatePost.jsx
+++ src/blog/pages/CreatePost.jsx
@@ -1,13 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { useNavigate } from 'react-router';
 import { useFetch } from '../../hooks/useFetch';
 import { useForm } from '../../hooks/useForm';
+import JoditEditor from 'jodit-react';
 
 export const CreatePost = () => {
 
     const navigate = useNavigate();
 
     // TODO: Recoger desde el token el id del usuario editor
+    // React Jodit WYSIWYG Editor
+    const editor = useRef(null);
+    const [content, setContent] = useState('');
+
+    const config = useMemo(
+        () => ({
+            readonly: false, // all options from https://xdsoft.net/jodit/docs/,
+            //width: '100%',
+            toolbarButtonSize: "small",
+            style: {
+                color: '#0c0c0c'
+            }
+        }),
+        []
+    );
 
     const formatDate = () => {
         const dateObj = new Date();
@@ -37,7 +53,15 @@ export const CreatePost = () => {
                 <input type="text" name="post_title" id="post_title" placeholder='Titulo de la noticia' />
                 <input type='text' name='post_subtitle' id="post_subtitle" placeholder='Subtitulo de la noticia' />
                 {/* TODO: Usar react det text = React Jodit WYSIWYG Editor */}
-                <input type="text" name="post_content" id="post_content" placeholder='Contenido de la noticia' />
+                <JoditEditor
+                    ref={editor}
+                    value={content}
+                    config={config}
+                    tabIndex={1}
+                    name="post_content" id="post_content"
+                    placeholder='Contenido de la noticia'
+                />
+                {/*<input type="text" name="post_content" id="post_content" placeholder='Contenido de la noticia' />*/}
                 <input type="hidden" name="date_insert" id='date_insert' value={formatDate()} />
 
                 <button type="submit">Crear oticia</button>
